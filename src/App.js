b3e0984f087fea9176a5a210d5864a4ac850d4af
elpiedra83 <luis.piedrahita@outlook.com>
2021-03-04 13:24:04 -0300

    first upload with basic functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { render } from "react-dom";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import "./App.css";
+import gfm from "remark-gfm";
 
 function App() {
+  const [markdown, setMarkdown] = useState("# sup");
+  const renderers = {
+    code: ({ language, value }) => {
+      return (
+        <SyntaxHighlighter style={dark} language={language} children={value} />
+      );
+    },
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        id="editor"
+        onChange={(e) => setMarkdown(e.target.value)}
+        value={markdown}
+      />
+      <ReactMarkdown
+        id="preview"
+        plugins={[gfm]}
+        source={markdown}
+        renderers={renderers}
+      />
     </div>
   );
 }
